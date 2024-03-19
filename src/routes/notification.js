b80578489b3f5ece7e59105d7772e6682eb4bc99
yunseo10987 <yunseo10987@naver.com>
2024-03-19 02:22:06 +0000

    feat: 알림 읽기 기능 추가

---
+++ src/routes/notification.js
@@ -0,0 +1,34 @@
+const router = require("express").Router()
+const validator = require('../utils/validator')
+const notification = require('../mongooseSchema/notificationsSchema')
+
+router.get("/", async (req, res) => {
+    const result = {
+        "success": false,
+        "message": "",
+        "data": ""
+    }
+    try{
+        validator.session(req.session.idx)
+        const notifications = await notification.find({
+            $or: [
+                {"type": "global"},
+                {"type": "individual",
+                "receiver": req.session.idx
+                }
+            ]
+        }) 
+        result.success = true
+        result.data = notifications
+
+    }catch(e){
+        console.log(e.message)
+        result.message = e.message;
+    }finally{
+        res.send(result);
+    }
+
+})
+
+
+module.exports = router
