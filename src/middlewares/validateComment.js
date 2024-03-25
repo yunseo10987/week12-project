010de35eee2adf5888916048666becfaa5b26eb0
yunseo10987 <yunseo10987@naver.com>
2024-03-25 08:16:03 +0000

    댓글 유효성 검사 미들웨어 추가

---
+++ src/middlewares/validateComment.js
@@ -0,0 +1,13 @@
+const validateComment = (req, res, next) => {
+  const { content } = req.body;
+
+  if (content === null || content === undefined || content === "") {
+    throw new Error("내용을 입력하세요.");
+  }
+
+  if (content.length < 1 || content.length > 1500) {
+    throw new Error("내용은 1500자 이내로 작성하세요");
+  }
+};
+
+module.exports = validateComment;
